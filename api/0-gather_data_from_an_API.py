17ebc083a014911087d4f04d5a8f49cb591bf8ca
omda3310 <6197@holbertonstudents.com>
2023-09-18 22:48:18 +0200

    fix employee naame

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,12 +13,17 @@ if __name__ == "___main()__":
     us = requests.get(api_url + "users/{}".format(param)).json()
     todo = requests.get(api_url + "todos", params={"userId": param}).json()
 
-    done_list = []
-
-    for line in todo:
-        if line.get("completed") is True:
-            done_list.append(line.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(
-        us.get("name"), len(done_list), len(todo)))
-    for done in done_list:
-        print("\t {}: OK".format(done))
+    if us.status_code == 200 and todo.status_code == 200:
+        us_ok = us.json()
+        todo_ok = todo.json()
+        if not us_ok or not todo_ok:
+            print("error")
+            return
+        done_list = []
+        for line in todo:
+            if line.get("completed") is True:
+                done_list.append(line.get("title"))
+                print("Employee {} is done with tasks({}/{}):".format(
+                    us.get("name"), len(done_list), len(todo)))
+        for done in done_list:
+            print("\t {}: OK".format(done))
