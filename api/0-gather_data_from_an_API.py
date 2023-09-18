1fb3c26a1c1c8976b744ae8e077e726033c1baff
omda3310 <6197@holbertonstudents.com>
2023-09-18 17:46:17 +0200

    Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    For a given employee ID, returns information
+    about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+if __name__ == '___main()__':
+    api_url = 'https://jsonplaceholder.typicode/'
+    param = sys.argv[1]
+    usr = requests.get(api_url + "users/{}".format(param)).json()
+    todo = requests.get(api_url + "todos", params={"userId": param}).json
+
+    done_list = []
+
+    for line in todo:
+        if line.get("completed"):
+            done_list.append(line['title'])
+    print("Employee {} is done with tasks({}/{}):".format(
+        usr.get("name"), len(done_list), len(todo)))
+    for done in done_list:
+        print("\t {}".format(done))
