b331404384e59d5d2e553fffba501bebdbfe69ca
omda3310 <6197@holbertonstudents.com>
2023-09-22 12:47:05 +0200

    test

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,16 +8,14 @@ import sys
 if __name__ == "__main__":
     api_url = "https://jsonplaceholder.typicode.com/"
     inp = int(sys.argv[1])
-    us_ok = requests.get(f'{api_url}users/{inp}').json()
-    username = us_ok.get("name")
-    todo_oka = requests.get(f'{api_url}todos', params={"userId": inp})
-    todo_ok = todo_oka.json()
+    us_ok = requests.get(api_url + "users/{}".format(inp)).json()
+    todo_ok = requests.get(api_url + "user/{}/todos".format(inp)).json()
 
     done_list = []
     for t in todo_ok:
         if t.get("completed") is True:
             done_list.append(t.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(username,
+    print("Employee {} is done with tasks({}/{}):".format(us_ok.get("name"),
           len(done_list), len(todo_ok)))
 
     for done in done_list:
