628597cb49c9ac4cb0f493d9226a77cb5e214bc6
omda3310 <6197@holbertonstudents.com>
2023-09-24 09:43:48 +0200

    version2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,43 +1,24 @@
 #!/usr/bin/python3
 """
-This script uses the JSON placeholder API to query data about an employee.
+Uses the JSON placeholder api to query data about an employee
 """
 
-import requests
+from requests import get
 from sys import argv
 
-URL = 'https://jsonplaceholder.typicode.com'
-
-
-def get_name(user_id):
-    """Fetch user by ID."""
-    resp = requests.get(f"{URL}/users/{user_id}")
-    user_name = resp.json()
-    return user_name.get('name')
-
-
-def get_todos(user_id):
-    resp = requests.get(f"{URL}/todos", params={'userId': user_id})
-    todos = resp.json()
-    return todos
-
-
-def display_infos(user_id):
-    try:
-        user_name = get_name(user_id)
-        todos = get_todos(user_id)
-        num_todos = len(todos)
-        completed_todos = [t for t in todos if t.get("completed")]
-        print("Employee {} is done with tasks({}/{}):".format(
-            user_name, len(completed_todos), num_todos))
-        for t in completed_todos:
-            print(f"\t {t['title']}")
-    except requests.RequestException as ex:
-        print(f"Error: {ex}")
-
-
 if __name__ == '__main__':
-    if len(argv) < 2:
-        exit(1)
-    user_id = int(argv[1])
-    display_infos(user_id)
+    URL = 'https://jsonplaceholder.typicode.com'
+    td_url = URL + "/user/{}/todos".format(argv[1])
+    n_url = URL + "/users/{}".format(argv[1])
+    td_result = get(td_url).json()
+    n_result = get(n_url).json()
+
+    todo_count = len(td_result)
+    todo_complete = len([todo for todo in td_result
+                         if todo.get("completed")])
+    name = n_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_count))
+    for todo in td_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
