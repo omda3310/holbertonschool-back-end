f34aea18130f4f1550bfe35a06a8c38c310a5bc3
omda3310 <6197@holbertonstudents.com>
2023-10-18 18:50:58 +0200

    All to json

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+This script uses the JSON placeholder API to query data about an employee.
+"""
+import json
+import requests
+from sys import argv
+URL = 'https://jsonplaceholder.typicode.com'
+
+
+def get_users():
+    """Fetch user by ID."""
+    resp = requests.get(f'{URL}/users/{id}')
+    resp.raise_for_status()
+    return resp.json()
+    
+
+
+def get_todos(id):
+    """Fetch TODO and return ID"""
+    resp = requests.get(f'{URL}/todos', params={'userId': id})
+    resp.raise_for_status()
+    todos = resp.json()
+    return todos
+
+
+def export_all_to_json():
+    """Export alll to json"""
+    all_data = {}
+
+    try:
+        users = get_users()
+        for user in users:
+            user_id = user['id']
+            username = user['username']
+            todos = get_todos(user_id)
+
+            task_list = []
+            for t in todos:
+                t_data = {
+                    "username": username,
+                    "task": t['title'],
+                    "completed": t['completed']
+                }
+                task_list.append(t_data)
+
+            all_data[str(user_id)] = task_list
+
+        with open("todo_all employees.json", "w")as json_file:
+            json.dump(all_data, json_file)
+        
+    
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    export_all_to_json()
